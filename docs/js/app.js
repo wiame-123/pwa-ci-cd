ade941a22d467bde598df34b3108084662b1b545
wiam-1 <132479007+wiam-1@users.noreply.github.com>
2025-06-28 18:50:33 +0100

    ajouter runners

--- docs/js/app.js
+++ docs/js/app.js
@@ -58,7 +58,6 @@ if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
       .register("/serviceWorker.js")
-
       .catch(err => console.log("service worker not registered", err));
   });
 }
